e31ff86bcba7c19b67cbeced3f220fba7b5d1242
Sebastien <sebastien.andre.seb@gmail.com>
2017-12-19 22:59:05 +0100

    dd

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,22 +6,3 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
-
-require('zone.js/dist/zone-node');
-const express = require('express');
-const path = require('path');
-const { enableProdMode } = require('@angular/core');
-const { renderModuleFactory } = require('@angular/platform-server');
-const { AppServerModuleNgFactory } = require('../dist-ssr/main.bundle');
-enableProdMode();
-const index = require('fs')
-  .readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf8')
-  .toString();
-const app = express();
-app.get('**', function(req, res) {
-  renderModuleFactory(AppServerModuleNgFactory, {
-    url: req.path,
-    document: index
-  }).then(html => res.status(200).send(html));
-});
-exports.ssr = functions.https.onRequest(app);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
+import { CloudinaryModule, CloudinaryConfiguration } from '@cloudinary/angular-5.x';
+import { Cloudinary } from 'cloudinary-core';
 
 const APP_ID = 'angular-universal-firebase';
 
@@ -18,6 +20,7 @@ const APP_ID = 'angular-universal-firebase';
     BrowserModule,
     AppRoutingModule,
     BrowserModule.withServerTransition({ appId: APP_ID }),
+    CloudinaryModule.forRoot({Cloudinary}, { cloud_name: 'dqismn81g' } as CloudinaryConfiguration),
     HttpClientModule
   ],
   providers: [],
