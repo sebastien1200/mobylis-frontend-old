10880c5da8616d236c8d3a92f297da5901d84ead
Sebastien <sebastien.andre.seb@gmail.com>
2017-12-20 20:43:52 +0100

    ff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
+export * from './server-side-rendering'
+export * from './https-request'
+export * from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // https://firebase.google.com/functions/write-firebase-functions
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
