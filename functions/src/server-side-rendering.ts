dab1381eb0a79a63a505d10cda7ff1a6af7e58a7
Sebastien <sebastien.andre.seb@gmail.com>
2017-12-20 22:22:25 +0100

    ff

--- functions/src/server-side-rendering.ts
+++ functions/src/server-side-rendering.ts
@@ -6,10 +6,13 @@ const { enableProdMode } = require('@angular/core');
 const { renderModuleFactory } = require('@angular/platform-server');
 const { AppServerModuleNgFactory } = require('../dist-ssr/main.bundle');
 enableProdMode();
+
 const index = require('fs')
   .readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf8')
   .toString();
+
 const app = express();
+
 app.get('**', function(req, res) {
   renderModuleFactory(AppServerModuleNgFactory, {
     url: req.path,
